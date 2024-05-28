3a02bb1a11a825f9dd6eda879b7929db141df511
Jake Holman <me@jakeisonline.com>
2024-05-28 22:07:51 +0100

    initial component config and implementation

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JakeMadeThis = () => {
+  return (
+    <span>
+      👋 Made by <a href="https://www.jakeisonline.com" target="_blank">Jake</a>
+    </span>
+  );
+};
+
+export default JakeMadeThis;
